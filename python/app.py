7cc9868a7ecb574485078dc780bf46201d6acb4c
lordslair <rvandepo+github@gmail.com>
2020-03-28 22:21:26 +0100

    Added route /tiles/{x}/{y}/{n}

--- python/app.py
+++ python/app.py
@@ -21,3 +21,11 @@ def send_pcs_info(pcs_id):
     return result
   else:
     return '{}'
+
+@app.route('/tiles/<int:x>/<int:y>/<int:n>')
+def send_tiles_info(x,y,n):
+  result = query_tiles_zone(x,y,n) # result will be a JSON
+  if result:
+    return result
+  else:
+    return '{}'
--- python/queries.py
+++ python/queries.py
@@ -26,8 +26,37 @@ def query_fetchone(SQL,param,dict):
     else:
         return None
 
+# Meta Query using one parameter, and a fetchall()
+def query_fetchone(SQL,param,dict):
+    db             = sqlite3.connect(db_name, timeout=20)
+    if dict: db.row_factory = dict_factory # To force output as a dict
+    cursor         = db.cursor()
+
+    cursor.execute(SQL, (param,))
+    result = cursor.fetchall()
+
+    cursor.close()
+    db.close()
+    if result:
+        # We dump result into a JSON
+        # ensure_ascii is used to avoid unicode and have UTF-8
+        return json.dumps(result, ensure_ascii=False)
+    else:
+        return None
+
 def query_pcs_id(pcs_id):
     SQL     = """SELECT * FROM pcs WHERE id = ?"""
     result = query_fetchone(SQL,pcs_id,True)
+    if result:
+        return result
+
+def query_tiles_zone(x,y,n):
+    SQL     = """SELECT * \
+                 FROM tiles \
+                 WHERE ( \
+                           ABS(? - tiles.x) + ABS(? - tiles.y) \
+                         + ABS((- ? - ?) - (- tiles.x - tiles.y))
+                       ) / 2 <= ?"""
+    result = query_fetchall(SQL,(x,y,x,y,n),True)
     if result:
         return result
