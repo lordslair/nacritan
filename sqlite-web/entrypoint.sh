cf1b62d1816e3e1fb587cffed4e84f729354b6d3
lordslair <rvandepo+github@gmail.com>
2020-03-29 16:49:19 +0200

    Added sqlite-web entrypoint

---
+++ sqlite-web/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "`date +"%F %X"` Building Python dependencies and system set-up ..."
+
+apk update --no-cache \
+    && apk add --no-cache python3 \
+    && apk add --no-cache --virtual .build-deps \
+                                    python3-dev \
+                                    libffi-dev \
+                                    gcc \
+                                    libc-dev \
+                                    tzdata \
+    && pip3 --no-cache-dir install -U sqlite-web \
+    && cp /usr/share/zoneinfo/Europe/Paris /etc/localtime \
+    && apk del .build-deps
+
+echo "`date +"%F %X"` Build done ..."
+
+exec sqlite_web --host=$FLASK_HOST \
+                --port=$FLASK_PORT \
+                --password=$SQLITE_WEB_PASSWORD \
+                $SQLITE_DB_NAME
