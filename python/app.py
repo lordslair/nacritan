15b06d4c91b30de19a09008cc8f36dc16054b5e8
lordslair <rvandepo+github@gmail.com>
2020-03-31 15:35:00 +0200

    Added dependency

--- python/app.py
+++ python/app.py
@@ -2,7 +2,7 @@
 
 from queries    import *
 from functions  import funct_greet
-from flask      import Flask
+from flask      import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
