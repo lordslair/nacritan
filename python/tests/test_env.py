963dc224f144bba07d0f3b5da0e0f1526e8f1434
lordslair <rvandepo+github@gmail.com>
2020-04-06 17:51:36 +0200

    Added UT for env variables (tokens)

---
+++ python/tests/test_env.py
@@ -0,0 +1,11 @@
+import os
+
+# Pre-flight check to ensure token list exists
+def test_nacritan_pytest_tokens_exists():
+    tokens = eval(os.environ['AUTH_TOKENS'])
+    assert tokens
+
+# Pre-flight check to ensure PyTest token is present
+def test_nacritan_pytest_token_exists():
+    tokens = eval(os.environ['AUTH_TOKENS'])
+    assert 'PyTest' in tokens.values()
