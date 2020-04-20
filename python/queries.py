4385fa218f6d2a8d0c45bd38f568a3e7389b7ba5
lordslair <5469390+lordslair@users.noreply.github.com>
2020-04-20 16:27:44 +0200

    Modified INSERT/UPDATE for pcsInfos

--- python/queries.py
+++ python/queries.py
@@ -213,15 +213,29 @@ def query_insert_pc(rawjson,user):
     if rawjson['id']:
         if rawjson['race'] is None: rawjson['race'] = '' # Dirty fix waiting for race to be populated
         # INSERT OR REPLACE INTO pcsInfos
-        SQL_pcinfos = ("""INSERT OR REPLACE INTO pcsInfos ( id, name, race, img, dla, pas, pos, xp, xpMax, user )
-                         VALUES (
-                                 ?, ?,
-                                 COALESCE(?, (SELECT race FROM pcsInfos WHERE id = {} )),
-                                 ?, ?, ?, ?, ?, ?, ?
-                                )""").format(rawjson['id'])
+        SQL_pcinfos = """INSERT INTO pcsInfos ( id, name, race, img, dla, pas, pos, xp, xpMax, user )
+                         VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )
+                         ON CONFLICT(id)
+                         DO UPDATE SET img   = ?,
+                                       dla   = ?,
+                                       pas   = ?,
+                                       pos   = ?,
+                                       xp    = ?,
+                                       xpMax = ?,
+                                       user  = ?,
+                                       date  = datetime('now');"""
+
         result_pcinfos =  query_insert(SQL_pcinfos,
-                                       (rawjson['id'], rawjson['name'], rawjson['race'], rawjson['img'], rawjson['dla'],
-                                        rawjson['pas'], rawjson['pos'], rawjson['xp'], rawjson['xpMax'], user))
+                                       (rawjson['id'],
+                                        rawjson['name'],
+                                        rawjson['race'],  rawjson['img'],
+                                        rawjson['dla'],   rawjson['pas'],
+                                        rawjson['pos'],   rawjson['xp'],
+                                        rawjson['xpMax'], user,
+                                        rawjson['img'],
+                                        rawjson['dla'],   rawjson['pas'],
+                                        rawjson['pos'],   rawjson['xp'],
+                                        rawjson['xpMax'], user))
 
         if rawjson['caracs']:
             # INSERT OR REPLACE INTO pcsCaracs
