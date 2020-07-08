0dcd19bcdd8c43e0f97910226c7edd5aff40c97e
lordslair <5469390+lordslair@users.noreply.github.com>
2020-07-08 18:04:06 +0200

    Modified to handle pip3 from alpine:3.11

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -4,6 +4,7 @@ echo "`date +"%F %X"` Building Python dependencies and system set-up ..."
 
 apk update --no-cache \
     && apk add --no-cache python3 \
+                          py3-pip \
                           libjpeg \
     && apk add --no-cache --virtual .build-deps \
                                     python3-dev \
@@ -13,15 +14,14 @@ apk update --no-cache \
                                     jpeg-dev \
                                     zlib-dev \
                                     tzdata \
-    && pip3 --no-cache-dir install -U Flask \
-                                      Flask-cors \
-                                      Flask-httpauth \
-                                      Pillow-PIL \
-                                      redis \
-                                      pytest \
+    && pip --no-cache-dir install -U Flask \
+                                     Flask-cors \
+                                     Flask-httpauth \
+                                     Pillow-PIL \
+                                     redis \
+                                     pytest \
     && cp /usr/share/zoneinfo/Europe/Paris /etc/localtime \
-    && apk del .build-deps \
-    && pytest /code/tests
+    && apk del .build-deps
 
 echo "`date +"%F %X"` Build done ..."
 
