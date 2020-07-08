cea99421a5dcbce2ccd3e942ca25eba43e49af9b
lordslair <5469390+lordslair@users.noreply.github.com>
2020-07-08 22:25:07 +0200

    Fixed Typo

--- backup/entrypoint.sh
+++ backup/entrypoint.sh
@@ -14,8 +14,8 @@ apk update \
                                     tzdata \
     && cp /usr/share/zoneinfo/Europe/Paris /etc/localtime \
     && apk del .build-deps \
-    && wget "$GIT/backup/cron-backup-sh.daily"   -O /etc/periodic/hourly/cron-backup-sh  \
-    && wget "$GIT/backup/cron-backup-sh.hourly"  -O /etc/periodic/daily/cron-backup-sh   \
+    && wget "$GIT/backup/cron-backup-sh.daily"   -O /etc/periodic/daily/cron-backup-sh  \
+    && wget "$GIT/backup/cron-backup-sh.hourly"  -O /etc/periodic/hourly/cron-backup-sh   \
     && wget "$GIT/backup/cron-backup-sh.monthly" -O /etc/periodic/monthly/cron-backup-sh \
     && chmod 755 /etc/periodic/*/cron-backup-sh \
 
